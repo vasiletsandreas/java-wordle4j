74f3fb88723ae1534a8f68c483d066d5e1705e65
Andrey Vasilets <vasiletsandreas@yandex.ru>
2026-01-13 00:22:20 +0300

    7й проект v6

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum;
 
 import java.io.PrintWriter;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
